c1432d4a09cdff07b473b359507a0f07430d8afc
Prosper Nnadi <prospernnadi3@gmail.com>
2021-08-24 11:22:54 +0100

    Objects

---
+++ c3/index.js
@@ -0,0 +1,8 @@
+let company = {
+    name: 'ProyceTech',
+    year: 2023,
+    sector: 'Tech',
+    founder: 'Prosper',
+    isFutureCompany() {return this.year >= 2021 }
+  }
+  console.log(company);
