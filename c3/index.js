8b88f4facd483c86813c278ba6c2ea4e38eaf52b
Prosper Nnadi <prospernnadi3@gmail.com>
2021-08-24 11:25:40 +0100

    Objects

--- c3/index.js
+++ c3/index.js
@@ -1,8 +1,15 @@
-let company = {
-    name: 'ProyceTech',
-    year: 2023,
-    sector: 'Tech',
-    founder: 'Prosper',
-    isFutureCompany() {return this.year >= 2021 }
-  }
-  console.log(company);
+// let company = {
+//     name: 'ProyceTech',
+//     year: 2023,
+//     sector: 'Tech',
+//     founder: 'Prosper',
+//     isFutureCompany() {return this.year >= 2021 }
+//   }
+//   console.log(company);
+
+var scoops = 8;
+while (scoops > 0) {
+document.write("Another scoop!<br>");
+scoops = scoops - 1;
+}
+document.write("Life without ice cream isn't the same");
